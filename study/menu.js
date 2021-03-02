e64cef0810cf3ecb24098d6bfd8eabfd22cc43d4
wangzhux037 <78311198+wangzhux037@users.noreply.github.com>
2021-03-01 23:26:06 -0500

    Delete study directory

--- study/menu.js
+++
@@ -1,9 +0,0 @@
-function showmenu(){
-	
-	document.getElementById('menuoverlay').style.display="block";
-}
-function hidemenu(){
-	
-	document.getElementById('menuoverlay').style.display="none";
-	
-}
